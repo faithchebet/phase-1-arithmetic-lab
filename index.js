f0ace98e725ead0349a86edead0a304e611d6a1b
faithchebet <faith.chebet@student.moringaschool.com>
2024-01-16 18:37:50 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// Write your code herevar num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2; 
+
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; 
+
+var num3 = 18;
+var num4 = 7;
+var mod = num3 % num4; 
+
+var numberSet = [5, 12, 8, 20, 15];
+var max = Math.max(...numberSet); 
+
+
+console.log("Multiply:", multiply);
+console.log("Random:", random);
+console.log("Mod:", mod);
+console.log("Max:", max);
